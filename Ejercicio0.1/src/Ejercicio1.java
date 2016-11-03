ba4b285de03de02b5317fccd6b8e7db838852394
Javier <javier.mf.1996@gmail.com>
2016-11-03 19:24:30 +0100

    ejercicio1

---
+++ Ejercicio0.1/src/Ejercicio1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		int A,B,resultado = 0;
+		int indice=A++;	//recorre valores desde a+1 hasta b-1
+		
+		System.out.print("Valor A:");
+		A=teclado.nextInt();
+		
+		System.out.print("Valor B:");
+		B=teclado.nextInt();
+		
+		
+		 while (indice <= B-1) {
+			 resultado=indice+resultado;
+			 indice=indice+1;
+		 }
+	
+		 System.out.println(resultado);
+		 
+		 
+		 
+		
+		 }
+			 
+		 
+		
+		
+	}
+
+
+
